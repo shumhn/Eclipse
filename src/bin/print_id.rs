f72e940d779825dda3543aa9719752b160a559f8
shumhn <theshumanhere@gmail.com>
2026-06-15 02:39:29 +0545

    chore: delete legacy scripts and rebrand to Eclipse
    - Deleted: fix_ts.js, dump-config.ts, test-pda.ts, init_repo.sh
    - Deleted: scripts/ (DAC mint setup, dark market creation scripts)
    - Deleted: src/bin/print_id.rs
    - Renamed root package to eclipse-prediction-market

--- src/bin/print_id.rs
+++
@@ -1,6 +0,0 @@
-use anchor_lang::prelude::*;
-use std::str::FromStr;
-
-fn main() {
-    println!("ID: {}", ephemeral_rollups_sdk::id());
-}
--- test-pda.ts
+++
@@ -1,6 +0,0 @@
-import { PublicKey } from '@solana/web3.js';
-const PROGRAM_ID = new PublicKey('ACPDQsNr539BaFTc1hRtryj66N7UH2GGdKPx6wyiYSYx');
-const idBuf = Buffer.alloc(8);
-idBuf.writeBigUInt64LE(BigInt(0));
-const [marketPda] = PublicKey.findProgramAddressSync([Buffer.from('market'), idBuf], PROGRAM_ID);
-console.log("Market 0 PDA:", marketPda.toBase58());
